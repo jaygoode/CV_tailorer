40f15de16f3606c1d4a04212af7be2dc41870d75
johnny <john_newcreek@protonmail.com>
2025-11-28 00:33:30 +0200

    updated deprecated import and added yaml with cv data text

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from langchain_community.chat_models import ChatOllama
+# from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
+import yaml 
 
 class CVText(BaseModel):
     job_experience: str = Field(..., description="The improved CV job experience text.")
     skills: str = Field(..., description="Improved skills section.")
     model: str = Field(default="llama2", description="The AI model used to generate the CV content.")
 
+def read_yaml_file(filepath: str) -> dict:
+    """
+    Read and parse a YAML file.
 
-def tailor_cv_text(job_desc: str, cv_text: dict) -> CVText:
+    Args:
+        filepath (str): Path to the YAML file.
+
+    Returns:
+        dict: Parsed YAML data.
+    """
+
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+    return data
+
+def tailor_cv_text(job_desc: str, cv_job_experience_text: str, cv_skills_text:str) -> CVText:
     """
     Takes job description + CV text and tailors the CV to fit the role better.
     Returns parsed CVText object.
@@ -57,6 +73,8 @@ if __name__ == "__main__":
         "job_experience": "3 years Python RPA developer; automation work; some cybersecurity courses.",
         "skills": "Python, Linux, HTML, JS, some pentesting labs."
     }
-
-    updated = tailor_cv_text(job_desc, cv_text)
+    cv_data_filepath = "./CV_data.yaml"
+    cv_data_dict = read_yaml_file(cv_data_filepath)
+    breakpoint()
+    updated = tailor_cv_text(cv_data_dict["job_application_description"], cv_data_dict["cv_job_experience_text"], cv_data_dict["cv_skills_text"])
     print(updated)
